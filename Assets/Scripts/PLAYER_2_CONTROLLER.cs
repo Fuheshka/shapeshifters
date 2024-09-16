b660e2b6f61ef441219afebb125e37ce92dc3908
Fuheshka <62994194+Fuheshka@users.noreply.github.com>
2024-09-16 16:32:36 +0300

    Movements

---
+++ Assets/Scripts/PLAYER_2_CONTROLLER.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLAYER_2_CONTROLLER : MonoBehaviour
+{
+    public float speed = 10f;
+    public float jumpForce = 5f;
+    private Rigidbody rb;
+    public Vector3 jump;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
